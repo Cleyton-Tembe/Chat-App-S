4e29606377a95e20eb744bb4a146721d21153eec
Cleyton Samuel Tembe <accesscodetalk3r@gmail.com>
2025-12-03 23:20:38 +0200

    Updated the cors to accept render.com url also

--- BackEnd/src/Server/server.js
+++ BackEnd/src/Server/server.js
@@ -17,7 +17,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chat-app-s-1.onrender.com"],
     credentials: true,                
 }))
 app.use(express.json())
